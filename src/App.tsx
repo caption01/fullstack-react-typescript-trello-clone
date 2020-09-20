c55d4da996dc48d5d47ebfc47ea1859075deb104
nutchapon.h <nutchapon@enres.com>
2020-09-20 16:25:45 +0700

    trello-clone - first setup appStateContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-import { Card } from "./Card";
 import { Column } from "./Column";
 import { AddNewItem } from "./AddNewItem";
 
 import "./App.css";
 import { AppContainer } from "./styles";
 
+import { useAppState } from "./AppStateContext";
+
 function App() {
+  const { state } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column key={list.id} index={i} text={list.text} />
+      ))}
       <AddNewItem onAdd={console.log} toggleButtonText="+ Add anther list" />
     </AppContainer>
   );
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { Card } from "./Card";
 import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./AppStateContext";
 
 interface ColumnsProps {
   text: string;
+  index: number;
 }
 
 export const Column = ({
   text,
+  index,
   children,
 }: React.PropsWithChildren<ColumnsProps>) => {
+  const { state } = useAppState();
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
       <AddNewItem
         onAdd={console.log}
         toggleButtonText="+ Add anther task"
